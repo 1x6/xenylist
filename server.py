5de040e0054e58c4500b1bd6271bc1cf680478b0
xeny <44981148+1x6@users.noreply.github.com>
2022-08-15 12:14:22 +0100

    uhhhh fixes again

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from flask import Flask, request, Response, jsonify
 
 app = Flask(__name__)
 
+
 def latest_activity(id, progress, media):
 
     postme = {"query":"query media($id:Int,$type:MediaType,$isAdult:Boolean){Media(id:$id,type:$type,isAdult:$isAdult){id title{userPreferred romaji english native}coverImage{extraLarge large}bannerImage startDate{year month day}endDate{year month day}description season seasonYear type format status(version:2)episodes duration chapters volumes genres synonyms source(version:3)isAdult isLocked meanScore averageScore popularity favourites isFavouriteBlocked hashtag countryOfOrigin isLicensed isFavourite isRecommendationBlocked isFavouriteBlocked isReviewBlocked nextAiringEpisode{airingAt timeUntilAiring episode}relations{edges{id relationType(version:2)node{id title{userPreferred}format type status(version:2)bannerImage coverImage{large}}}}characterPreview:characters(perPage:6,sort:[ROLE,RELEVANCE,ID]){edges{id role name voiceActors(language:JAPANESE,sort:[RELEVANCE,ID]){id name{userPreferred}language:languageV2 image{large}}node{id name{userPreferred}image{large}}}}staffPreview:staff(perPage:8,sort:[RELEVANCE,ID]){edges{id role node{id name{userPreferred}language:languageV2 image{large}}}}studios{edges{isMain node{id name}}}reviewPreview:reviews(perPage:2,sort:[RATING_DESC,ID]){pageInfo{total}nodes{id summary rating ratingAmount user{id name avatar{large}}}}recommendations(perPage:7,sort:[RATING_DESC,ID]){pageInfo{total}nodes{id rating userRating mediaRecommendation{id title{userPreferred}format type status(version:2)bannerImage coverImage{large}}user{id name avatar{large}}}}externalLinks{id site url type language color icon notes isDisabled}streamingEpisodes{site title thumbnail url}trailer{id site}rankings{id rank type format year season allTime context}tags{id name description rank isMediaSpoiler isGeneralSpoiler userId}mediaListEntry{id status score}stats{statusDistribution{status amount}scoreDistribution{score amount}}}}",
@@ -104,7 +105,7 @@ def add_media():
 
         r = requests.post("https://graphql.anilist.co", json=postme)
         resp = r.json()
-
+        print(resp)
         feeds = []
 
         with open(f"data/xeny/{media_type}.json", "r") as f:
@@ -118,7 +119,7 @@ def add_media():
 
         _dict["title"] = resp["data"]["Media"]["title"]["english"]
         if resp["data"]["Media"]["title"]["english"] == None:
-            _dict["title"] = resp["media"]["title"]["romaji"]
+            _dict["title"] = resp["data"]["Media"]["title"]["romaji"]
         _dict["media_id"] = resp["data"]["Media"]["id"]
         _dict["status"] = "planning"
         _dict["score"] = 0
